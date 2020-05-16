f87179bbb6dca930c979352f8b186dd7a81ec56c
zenchenkoyaroslav <47921331+zenchenkoyaroslav@users.noreply.github.com>
2020-05-16 22:03:12 +0200

    init commit

---
+++ src/test/java/com/yz/moviereview/MovieReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yz.moviereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
