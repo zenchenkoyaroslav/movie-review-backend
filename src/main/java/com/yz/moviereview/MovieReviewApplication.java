56db5218a6894ade0e893bdbd7d64dec64dda66d
zenchenkoyaroslav <47921331+zenchenkoyaroslav@users.noreply.github.com>
2020-05-17 02:10:03 +0200

    add cors

--- src/main/java/com/yz/moviereview/MovieReviewApplication.java
+++ src/main/java/com/yz/moviereview/MovieReviewApplication.java
@@ -2,6 +2,10 @@ package com.yz.moviereview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
@@ -11,4 +15,13 @@ public class MovieReviewApplication {
 		SpringApplication.run(MovieReviewApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
