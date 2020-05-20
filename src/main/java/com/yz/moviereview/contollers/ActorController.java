f98db0a5d28fa1d8b913b3e6b530011f8106d2b1
zenchenkoyaroslav <47921331+zenchenkoyaroslav@users.noreply.github.com>
2020-05-20 20:57:17 +0200

    added CRUD Actor

--- src/main/java/com/yz/moviereview/contollers/ActorController.java
+++ src/main/java/com/yz/moviereview/contollers/ActorController.java
@@ -4,8 +4,11 @@ package com.yz.moviereview.contollers;
 import com.yz.moviereview.entities.Actor;
 import com.yz.moviereview.service.ActorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -24,5 +27,24 @@ public class ActorController extends Controller {
     @GetMapping("/{id}")
     public @ResponseBody Actor getActor(@PathVariable("id") Long id){
         return actorService.getActor(id);
+    }
+
+    @PostMapping
+    public @ResponseBody Actor newActor(@RequestBody Actor actor, HttpServletRequest request){
+        getUser(request);
+        return actorService.addActor(actor);
+    }
+
+    @PutMapping("/{id}")
+    public Actor updateActor(@PathVariable("id") Long id, @RequestBody Actor actor, HttpServletRequest request){
+        getUser(request);
+        return actorService.updateActor(id, actor);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteActor(@PathVariable("id") Long id, HttpServletRequest request){
+        getUser(request);
+        actorService.deleteActor(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/yz/moviereview/service/ActorService.java
+++ src/main/java/com/yz/moviereview/service/ActorService.java
@@ -1,13 +1,14 @@
 package com.yz.moviereview.service;
 
 import com.yz.moviereview.entities.Actor;
-import com.yz.moviereview.entities.Film;
+import com.yz.moviereview.exceptions.ValidationException;
 import com.yz.moviereview.repositories.ActorRepository;
-import com.yz.moviereview.repositories.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ActorService {
@@ -20,5 +21,38 @@ public class ActorService {
 
     public Actor getActor(Long id){
         return actorRepository.findById(id).orElse(null);
+    }
+
+    public Actor addActor(Actor actor){
+        Objects.requireNonNull(actor);
+        if (StringUtils.isEmpty(actor.getName())){
+            throw new ValidationException("Actor name is empty");
+        }
+        if (StringUtils.isEmpty(actor.getPhoto())){
+            throw new ValidationException("Photo url is empty");
+        }
+        return actorRepository.save(actor);
+    }
+
+    public Actor updateActor(Long oldId, Actor actor){
+        Objects.requireNonNull(actor);
+        if (StringUtils.isEmpty(actor.getName())){
+            throw new ValidationException("Actor name is empty");
+        }
+        if (StringUtils.isEmpty(actor.getPhoto())){
+            throw new ValidationException("Photo url is empty");
+        }
+        Actor fromDB = actorRepository.getOne(oldId);
+        if (fromDB == null){
+            throw new ValidationException("Old actor doesnt exists");
+        }
+        fromDB.setName(actor.getName());
+        fromDB.setPhoto(actor.getPhoto());
+        fromDB.setFilms(actor.getFilms());
+        return actorRepository.save(fromDB);
+    }
+
+    public void deleteActor(Long id){
+        actorRepository.deleteById(id);
     }
 }
