d1d50ed17efd46d4838f5b439fa9470e79744573
yaroslav <gityarik@gmail.com>
2020-05-23 19:14:10 +0200

    tests are working

--- src/main/java/com/yz/moviereview/config/ExceptionInterceptor.java
+++ src/main/java/com/yz/moviereview/config/ExceptionInterceptor.java
@@ -1,9 +1,11 @@
 package com.yz.moviereview.config;
 
+import com.yz.moviereview.exceptions.ValidationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -14,6 +16,11 @@ public class ExceptionInterceptor extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return new ResponseEntity<Object>("AS" , HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ ValidationException.class})
+    public ResponseEntity<Object> handleException() {
+        return new ResponseEntity<Object>("Client error" , HttpStatus.BAD_REQUEST);
     }
 
 }
--- src/test/java/com/yz/moviereview/contollers/UserControllerTest.java
+++ src/test/java/com/yz/moviereview/contollers/UserControllerTest.java
@@ -3,78 +3,80 @@ package com.yz.moviereview.contollers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yz.moviereview.entities.USERROLE;
 import com.yz.moviereview.entities.User;
+import com.yz.moviereview.repositories.UserRepository;
 import com.yz.moviereview.requests.UserRequest;
-import com.yz.moviereview.service.UserService;
+import org.hibernate.mapping.Any;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class UserControllerTest {
     private static final Long USER_ID_1 = 1L;
     private static final String USER_USERNAME_1 = "user";
     private static final String USER_NAME_1 = "name";
     private static final String USER_PASSWORD_1 = "password";
-    private static final String USER_PASSWORD_HASH_1 = "$2a$10$ITjRTdRw4E4n3aa2z0EWteNOavKfBZBl0dS4EkYtZiIKmcL2U9rbC";
+    private static final String USER_PASSWORD_HASH_1 = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
 
     @Autowired
     private MockMvc mvc;
 
-    @Autowired
     private ObjectMapper objectMapper;
 
-    @InjectMocks
-    private UserController userController;
+    @MockBean(name = "userRepository")
+    private UserRepository userRepository;
 
-    @InjectMocks
-    private UserService userService;
-
-    User user;
 
     @Before
     public void init(){
+        objectMapper = new ObjectMapper();
         MockitoAnnotations.initMocks(this);
-        user = new User();
+        User user = new User();
         user.setId(USER_ID_1);
         user.setName(USER_NAME_1);
         user.setUsername(USER_USERNAME_1);
         user.setPassword(USER_PASSWORD_HASH_1);
         user.setEmail("test@test.test");
         user.setRole(USERROLE.USER);
-        Mockito.when(userService.getUser(user.getId())).thenReturn(user);
+        Mockito.when(userRepository.findByUsername(USER_USERNAME_1)).thenReturn(user);
+        Mockito.when(userRepository.saveAndFlush(any())).thenReturn(user);
     }
 
     @Test
     public void loginSuccess() throws Exception {
+        UserRequest user = new UserRequest();
+        user.setUsername(USER_USERNAME_1);
+        user.setPassword(USER_PASSWORD_1);
         String content = objectMapper.writeValueAsString(user);
         MvcResult mvcResult = mvc.perform(post("/users/login").content(content).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk()).andReturn();
         content = mvcResult.getResponse().getContentAsString();
-        User response = objectMapper.readValue(content, User.class);
+        UserRequest response = objectMapper.readValue(content, UserRequest.class);
         assertNotNull(response.getToken());
     }
 
     @Test
     public void loginFailedWrongUsername() throws Exception {
         UserRequest user = new UserRequest();
-        user.setUsername(USER_USERNAME_1);
+        user.setUsername("WrongUsername");
         user.setPassword(USER_PASSWORD_1);
         String content = objectMapper.writeValueAsString(user);
         MvcResult mvcResult = mvc.perform(post("/users/login").content(content).contentType(MediaType.APPLICATION_JSON))
@@ -83,7 +85,7 @@ public class UserControllerTest {
 
     @Test
     public void loginFailedWrongPassword() throws Exception {
-        User user = new User();
+        UserRequest user = new UserRequest();
         user.setUsername(USER_USERNAME_1);
         user.setPassword("WrongPassword");
         String content = objectMapper.writeValueAsString(user);
