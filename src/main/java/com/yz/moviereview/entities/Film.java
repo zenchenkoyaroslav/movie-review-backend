ff492df62eecaa059f6ff838472ef661a749acea
yaroslav <gityarik@gmail.com>
2020-05-27 23:44:23 +0200

    fix commit

--- src/main/java/com/yz/moviereview/entities/Film.java
+++ src/main/java/com/yz/moviereview/entities/Film.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Table(name = "FILM")
 public class Film {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
